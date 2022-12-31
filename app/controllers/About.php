3f50724b08cc9be8cc6e2ceed4f792e06c8a3ec8
adiviaka <diva.adivia@gmail.com>
2023-01-01 03:12:53 +0700

    View

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,13 +1,21 @@
 <?php
 
-class About
+class About extends Controller
 {
     public function index($nama = 'Adivia K. A.', $umur = '20')
     {
-        echo "Hi! My name is $nama and I'm $umur years old.";
+        $data['nama'] = $nama;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About me';
+        $this->view('templates/header;', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header' . $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,6 @@
+    <title>About Page</title>
+    </head>
+
+    <body>
+        <h1>About me</h1>
+        <p>Hi! My name is <?= $data['nama']; ?> and I'm <?= $data['umur']; ?> years old</p>
